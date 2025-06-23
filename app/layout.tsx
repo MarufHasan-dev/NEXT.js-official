77f9676487128f651cab13838728bdd561ade798
MarufHasan-dev <170166811+MarufHasan-dev@users.noreply.github.com>
2025-06-23 14:58:01 +0600

    initial upload commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
 
 export default function RootLayout({
   children,
@@ -6,7 +7,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html className={`${inter.className} antialiased`} lang="en">
       <body>{children}</body>
     </html>
   );
